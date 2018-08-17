16a58746535b8a67840b58786e1cfb4ed2fe2230
Yaw Odame <yoodame@gmail.com>
2018-08-17 15:42:40 -0500

    let's try this out

---
+++ web/private/scripts/github/pushback.php
@@ -0,0 +1,14 @@
+<?php
+
+$ci_token="5f046b7165077b029b14f287676982def58491f4";
+$org="yoodame";
+$repo="pantheon_sync";
+$branch="master";
+
+$cmd="curl -u $ci_token: \
+      -d build_parameters[CIRCLE_JOB]=push_back \
+      https://circleci.com/api/v1.1/project/github/$org/$repo/tree/$branch";
+
+exec($cmd,$result);
+
+echo print_r($result);
